106332aa03dcec27d40efbc1c181dae79f0ee4ab
Glfx20 <jsgleisson@gmail.com>
2023-03-03 16:19:35 -0300

    a

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ import threading
 #Acessar o filtro salvo 
 
 filtro = [
-    "form-filtroAcss-dlgFilterPrefs-tableUser-37-j_idt349",
-    "form-filtroAcss-dlgFilterPrefs-tableUser-38-j_idt349"
+    "form-filtroAcss-dlgFilterPrefs-tableUser-37-j_idt349"
+
     ]
 
 data = funcoes.gerar_datas()
 
 x = [
-    threading.Thread(target=funcoes.definitiva, args=[filtro[0], data]),
-    threading.Thread(target=funcoes.definitiva, args=[filtro[1], data])
+    threading.Thread(target=funcoes.definitiva, args=[filtro[0], data])
 
      ]
 i = 0
