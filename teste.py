6f0e744ccfff36955d1f910b3be2eb5938f7bffb
Glfx20 <jsgleisson@gmail.com>
2023-06-10 17:06:21 -0300

    d

---
+++ teste.py
@@ -0,0 +1,6 @@
+# Qual será o resultado do código a seguir?
+
+
+lista = ['Maçã', 'Banana', 'Laranja', 'Melancia', 'Abacaxi']
+frutas = [fruta for fruta in lista if 'a' in fruta.lower()]
+print(frutas)
